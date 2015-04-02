bc937e924391ca385a7bcf6f86273b6da072e26e
Hubo <hubo@hubo-head.(none)>
2015-04-02 13:42:54 -0400

    Minor updates to the script, less errors on not recognizing a gesture

--- src/recognize.py
+++ src/recognize.py
@@ -21,21 +21,33 @@ loadClassifier()
 
 #Get the current gesture happening and classify it. Then print the result
 def classifyCurrentGesture(d):
+    #Make a kinect object to listen to the skeleton data
     user = Kinect(user=1)
     goodString = None
-    while goodString is None:
+    tries = 0
+    #Try for a number of times to get good data from the kinect object.
+    # If the kinect object gives good data we can make a good json string
+    while goodString is None and tries < 4:
         val =  user.get_posture()
         if val is not None:
             tup = (str(datetime.now().time()),  val)
             goodString = jsonMaker(tup)
         time.sleep(.25) 
-
+        tries += 1
+    
+    #If we went over the number of tries
+    if goodString is None:
+        return
+    
+    #Classify the dance based off of the angles generated from the kinect data.
     angles = generateAngles(goodString)
     classList = classifySample(angles)
     theDance = ""
     if len(classList) != 0:
         theDance = classList[0]
     #d[0] = 1
+    #This is a hacky way of IPC, I know it's slow but it works for now
+    safeRemove(".tmp.val")
     f = open(".tmp.val", "w")
     f.write(theDance)
     f.close()
@@ -45,10 +57,14 @@ def respondDance(res):
     #Switch on the result of the dance pose classificaiton
     #and perform an appropriate dance in response.
     robot = maestor()
-    f = open(".tmp.val", "r")
+    fileName = ".tmp.val"
+    if not os.path.exists(fileName):
+        return
+    f = open(fileName, "r")
     line = f.readline()
     result = line
     f.close()
+    safeRemove(fileName)
     print result
     if result == "Disco":
         stayAliveRobot(robot)
@@ -62,13 +78,17 @@ def respondDance(res):
         print "No dance recognized"
 
 
+def safeRemove(fileName):
+    try:
+        os.remove(fileName)
+    except OSError as e:
+        pass
 
 
 def main():
     keepGoing = True
     manager = Manager()
     d = manager.dict()
-    d['a'] = ""
     while keepGoing:
         print "Enter q to quit"
         print "Enter anything else to recognize and dance"
@@ -80,10 +100,9 @@ def main():
         p2 = Process(target=respondDance, args=(d,))
         p1.start()
         p1.join()
-        print d
         p2.start()
         p2.join()
 
 if __name__ == '__main__':
     main()
-    os.remove(".tmp.val")
+    safeRemove(".tmp.val")
